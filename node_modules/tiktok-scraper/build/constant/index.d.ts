9a868c31440dda1dd5c552a97238ba678fd9dfab
gusfahmi <gus@entertrim.com>
2021-01-05 16:26:45 +0700

    Updating script

---
+++ node_modules/tiktok-scraper/build/constant/index.d.ts
@@ -0,0 +1,12 @@
+declare const _default: {
+    scrape: string[];
+    history: string[];
+    sourceType: {
+        user: number;
+        music: number;
+        trend: number;
+    };
+    verifyFp: () => never;
+    userAgent: () => string;
+};
+export = _default;
